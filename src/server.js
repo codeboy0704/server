f5000c9e4450edbe911d5bf6294c2ba6ece32db3
Draigon07 <ian.flores2@utp.ac.pa>
2024-04-02 12:45:28 -0500

    new packages and config added

--- src/server.js
+++ src/server.js
@@ -1,16 +1,24 @@
 import express from 'express';
+import connect from "./connection.js"
+import { signin, signup } from './auth.js';
+import errorHandler from './errorHandler.js';
+import { config } from './config.js';
+import morgan from 'morgan';
+import cors from "cors"
+import productRouter from "./products/product.router.js"
+const dotenv = require('dotenv')
+dotenv.config()
 const app = express();
-const port = 3000;
-// this only possible in express version 4.16.0 onwards
-// it is used to get the parsed data
 app.use(express.json());
-app.use( (req, res, next)=>{
-  res.status(404);
-  if (req.accepts('json')) {
-    res.send({error: true, message: 'Route Not found' });
-    return;
-   }
-});
-app.listen(port, ()=>{
-  console.log('app is listening on port 3000');
+app.use(
+  cors()
+);
+app.use(morgan("dev"))
+app.use("/product", productRouter)
+app.post('/register', signup)
+app.use('/login', signin)
+app.use("/", errorHandler);
+app.listen(config.PORT, () => {
+  console.log(`app is listening on port ${config.PORT}`);
+  connect();
 });
